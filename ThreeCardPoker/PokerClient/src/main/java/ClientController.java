0c5cff2716e53afcb8933f00cc34663f15f3ac5b
asxvi <astanson02@gmail.com>
2025-11-11 18:37:58 -0600

    im confused

--- ThreeCardPoker/PokerClient/src/main/java/ClientController.java
+++ ThreeCardPoker/PokerClient/src/main/java/ClientController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
-
+//khalid
 public class ClientController {
     @FXML
     private  TextField ipTextField;
